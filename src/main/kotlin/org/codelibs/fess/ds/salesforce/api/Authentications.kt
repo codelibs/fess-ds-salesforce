bf9c7b4c538df4242d901c2afebe4706bcf9a2f7
Ryo Kuramoto <kuramoto@n2sm.net>
2019-03-20 14:14:11 +0900

    remove jwt library

--- src/main/kotlin/org/codelibs/fess/ds/salesforce/api/Authentications.kt
+++ src/main/kotlin/org/codelibs/fess/ds/salesforce/api/Authentications.kt
@@ -15,26 +15,25 @@
  */
 package org.codelibs.fess.ds.salesforce.api
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.google.api.client.util.Base64
 import com.google.api.client.util.SecurityUtils
 import com.sforce.async.BulkConnection
 import com.sforce.soap.partner.Connector
 import com.sforce.soap.partner.PartnerConnection
 import com.sforce.ws.ConnectorConfig
+import org.apache.commons.codec.binary.Base64
 import org.codehaus.jackson.annotate.JsonIgnoreProperties
 import org.codelibs.curl.Curl
 import org.codelibs.fess.ds.salesforce.SalesforceDataStoreException
 import java.io.InputStream
+import java.security.KeyFactory
+import java.security.Signature
 import java.security.interfaces.RSAPrivateKey
 import java.security.spec.PKCS8EncodedKeySpec
-import java.util.*
 
 internal const val BASE_URL = "https://login.salesforce.com"
 internal const val API_VERSION = "45.0"
@@ -93,15 +92,26 @@ internal fun getPrivateKey(privateKeyPem: String): RSAPrivateKey {
 }
 
 internal fun createJWT(username: String, clientId: String, privateKeyPem: String, baseUrl: String): String {
-    val privateKey = getPrivateKey(privateKeyPem)
-    val now = System.currentTimeMillis()
-    return JWT.create()
-            .withIssuer(clientId)
-            .withSubject(username)
-            .withAudience(baseUrl)
-            .withIssuedAt(Date(now))
-            .withExpiresAt(Date(now + 300 * 1000))
-            .sign(Algorithm.RSA256(null, privateKey))
+    val expire = (System.currentTimeMillis() / 1000) + 300
+    val header = """{"alg":"RS256"}"""
+    val payload = """{"iss": "$clientId", "sub": "$username", "aud": "$baseUrl", "exp": "$expire"}"""
+
+    val token = StringBuilder()
+    token.append(Base64.encodeBase64URLSafeString(header.toByteArray(Charsets.UTF_8)))
+    token.append(".")
+    token.append(Base64.encodeBase64URLSafeString(payload.toByteArray(Charsets.UTF_8)))
+
+    val keySpec = PKCS8EncodedKeySpec(Base64.decodeBase64(privateKeyPem))
+    val privateKey = KeyFactory.getInstance("RSA").generatePrivate(keySpec) as RSAPrivateKey
+    val signature = Signature.getInstance("SHA256withRSA")
+    signature.initSign(privateKey)
+    signature.update(token.toString().toByteArray(Charsets.UTF_8))
+    val signedPayload = Base64.encodeBase64URLSafeString(signature.sign())
+
+    token.append(".")
+    token.append(signedPayload)
+
+    return token.toString()
 }
 
 internal fun parseTokenResponse(content: InputStream): TokenResponse {
